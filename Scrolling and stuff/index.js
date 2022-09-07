c3c15ae50c82531477e9a95a47372dfdb2818a9d
Stiffpixels <saifimuzammil2@gmail.com>
2022-09-07 16:43:29 +0530

    did the smooth scrolling and back to top button

--- Scrolling and stuff/index.js
+++ Scrolling and stuff/index.js
@@ -6,6 +6,7 @@ const linksContainer = document.querySelector(".links");
 const dateYear =document.querySelector(".year");
 const date = new Date();
 const scrollLink = document.querySelectorAll(".scroll-link");
+const backBtn = document.querySelector(".back-to-top");
 
 dateYear.innerHTML = date.getFullYear();
 
@@ -14,8 +15,16 @@ window.onscroll = function(){
   if(window.pageYOffset>navBarHeight){
     //navBar.style.background= "#2c2c2c";
     navBar.style.position= "fixed";
+    navBar.style.boxShadow = "0 0 10px black";
   }else{
     navBar.style.position= "absolute";
+    navBar.style.boxShadow = "none";
+  }
+  
+  if(window.pageYOffset> 500){
+    backBtn.style.display= "block";
+  }else{
+  backBtn.style.display= "none";
   }
 }
 
@@ -29,9 +38,22 @@ exitBtn.addEventListener("click", function(){
   navEl.classList.remove("show-menu");
 });
 
+backBtn.addEventListener("click", function(){
+  window.scrollTo({
+    left:0, top:0
+  });
+});
+
 scrollLink.forEach(function (link) {
   link.addEventListener("click", function(e){
     navEl.classList.remove("show-menu");
     e.preventDefault();
+    const ident = link.getAttribute("href").slice(1);
+    const elem = document.getElementById(ident);
+    console.log(elem);
+    let pos = elem.offsetTop;
+    window.scrollTo({
+      left:0, top: pos-navBarHeight
+    });
   });
 });
